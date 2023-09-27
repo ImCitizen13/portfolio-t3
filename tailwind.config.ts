f902bc139deddcfe52f6474f659bccb7d82e4b57
ImCitizen13 <meltohamy@protonmail.com>
2023-09-27 15:28:02 +0400

    Added menu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+
+    },
+    fontFamily: {
+      'Ibarra': ['"Ibarra Real Nova"', 'serif']
+    }
+
   },
   plugins: [],
 } satisfies Config;
